846aafbee434b237a1db9c7599927b8eeb06c094
pratyushuniv2022-code <pratyush.univ2022@gmail.com>
2025-09-21 08:44:20 +0530

    Update streamlit_concrete_assistant.py

--- streamlit_concrete_assistant.py
+++ streamlit_concrete_assistant.py
@@ -1,5 +1,7 @@
 # streamlit_concrete_assistant_fast.py
 # Importing necessary library
+import os
+os.environ["STREAMLIT_WATCHDOG_IGNORE"] = "true"
 import streamlit as st
 from pathlib import Path
 import uuid
